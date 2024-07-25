062f5f4fa204188382f799ddeb6bc68786d2882b
Prashant Mishra <pm21641@gmail.com>
2024-07-25 10:56:43 +0530

    dayjs support

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import JSPdfVisualizer from "./JSPdfVisualizer";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./HomePage";
-
+import _ from "lodash";
+import dayjs from "dayjs";
 function App() {
+  useEffect(() => {
+    window._ = _;
+    window.dayjs = dayjs;
+  }, []);
+
   const [dark, setDark] = useState(
     localStorage?.getItem("darkMode") &&
       localStorage?.getItem("darkMode") != "undefined"
